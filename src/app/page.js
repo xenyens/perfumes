a76a5de9e72b243d140cb509ace3491fff1a40a5
xenyens <xpadilla@ensenada.tecnm.mx>
2023-12-10 17:33:20 -0800

    v1.1.6

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import { perfumeData } from "@/helper/data";
 const HomePage = () => {
   return (
     <main className=" mx-auto w-full md:w-[80%] shadow-lg md:rounded-md">
-      <div className="grid gap-5 md:grid-cols-2 lg:grid-cols-2 justify-center md:items-center lg:items-center md:p-12 lg:p-12">
+      <div className="grid gap-5 md:grid-cols-2 lg:grid-cols-3 justify-center md:items-center lg:items-center md:p-12 lg:p-12">
         {perfumeData.map((perfume, index) => (
           <Tarjeta
             key={index}
--- src/components/Tarjeta.jsx
+++ src/components/Tarjeta.jsx
@@ -12,23 +12,39 @@ export default function Tarjeta({
   capacidad,
 }) {
   return (
-    <div className="tarjeta flex flex-col items-center justify-center rounded-md p-12 min-w-[300px] min-h-[400px]">
-      <div>
+    <div className="tarjeta flex flex-col justify-center rounded-md p-12 max-w-[300px] h-[500px]">
+      <div className="space-y-3">
         <div>
           <p className="text-xl uppercase font-bold">{marca}</p>
           <p className="uppercase font-thin">{nombre}</p>
           <h4 className="font-bold text-xl">{currency(precio)}</h4>
           <h5>{capacidad} ml</h5>
         </div>
-        <small
-          className={`${
-            disponible ? "text-green-300" : "text-red-800"
-          } font-bold uppercase`}
-        >
-          {disponible ? "Disponible" : "Vendido"}
-        </small>
+
+        <div className="flex flex-col space-y-2">
+          <small
+            className={`${
+              disponible
+                ? "text-green-600 bg-green-200"
+                : "text-red-800 bg-red-100"
+            }  uppercase rounded-md p-2`}
+          >
+            {disponible ? "Disponible" : "Vendido"}
+          </small>
+
+          {disponible && (
+            <Link
+              className=" bg-red-400  rounded-md hover:bg-red-600 uppercase border-none"
+              href={`https://wa.me/526461954586?text=Hola que tal, me interesa este producto:${marca} - ${nombre} - $${precio}`}
+            >
+              <p className="text-sm p-2 font-semibold text-red-900">
+                INFORMACIÓN
+              </p>
+            </Link>
+          )}
+        </div>
       </div>
-      <div className="">
+      <div className="mt-3">
         <Image
           alt="perfume"
           className="object-cover rounded-xl"
@@ -36,13 +52,6 @@ export default function Tarjeta({
           width={270}
           height={270}
         />
-      </div>
-      <div className="p-2 bg-gray-400 mt-2 w-full rounded-lg text-center hover:bg-gray-600 hover:text-white">
-        <Link
-          href={`https://wa.me/526461954586?text=Hola que tal, me interesa este producto:${marca} - ${nombre} - $${precio}`}
-        >
-          Más info
-        </Link>
       </div>
     </div>
   );
